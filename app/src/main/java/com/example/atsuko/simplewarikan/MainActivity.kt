b97566430e4d2662b5338d0d66340c637d6d692c
kinnerapriyap <kinnerapriyap@gmail.com>
2019-08-22 15:13:58 +0900

    Text change

--- app/src/main/java/com/example/atsuko/simplewarikan/MainActivity.kt
+++ app/src/main/java/com/example/atsuko/simplewarikan/MainActivity.kt
@@ -11,12 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.calculate_button).setOnClickListener {
-            val cost = findViewById<EditText>(R.id.cost).text.toString()
-            val peopleCount = findViewById<EditText>(R.id.people).text.toString()
-
-            findViewById<TextView>(R.id.result).text =
-                    CostCalculator().calculateCost(cost, peopleCount)
-        }
     }
 }
