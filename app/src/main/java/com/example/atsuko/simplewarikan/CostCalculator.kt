6ca43ba5145ed2c36613aeece159a0b6a2c321d9
kinnerapriyap <kinnerapriyap@gmail.com>
2019-08-22 14:52:15 +0900

    Remove unused files

--- app/src/main/java/com/example/atsuko/simplewarikan/CostCalculator.kt
+++
@@ -1,9 +0,0 @@
-package com.example.atsuko.simplewarikan
-
-class CostCalculator {
-
-
-    fun calculateCost(cost: String, peopleCount: String): String {
-        return (cost.toInt() / peopleCount.toInt()).toString()
-    }
-}
