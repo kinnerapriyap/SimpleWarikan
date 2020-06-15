2504c14dad2fc74745a8116c82b8ce537d8a6a2b
Kinnera Priya <kinnerapriyap@gmail.com>
2020-06-15 19:42:29 +0900

    Release/1.0 (#21)
    Pull Request successfully merged

--- app/src/main/java/com/example/atsuko/simplewarikan/MainActivity.kt
+++ app/src/main/java/com/example/atsuko/simplewarikan/MainActivity.kt
@@ -11,5 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.calculate_button).setOnClickListener {
+            val cost = findViewById<EditText>(R.id.cost).text.toString()
+            val peopleCount = findViewById<EditText>(R.id.people).text.toString()
+
+            findViewById<TextView>(R.id.result).text =
+                    CostCalculator().calculateCost(cost, peopleCount)
+        }
     }
 }
